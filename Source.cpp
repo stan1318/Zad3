54361f0ce4fc2da14c1f345153217f496279914e
stan1318 <43621628+stan1318@users.noreply.github.com>
2018-10-28 20:29:20 +0200

    Initial commit

---
+++ Source.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	char a;
+	cout << "Type in a letter: ";
+		cin >> a;
+		switch (a){
+		case'a': cout << "A" << endl;
+			break;
+		case 'b':cout << "B" << endl;
+			break;
+		case'c': cout << "C" << endl;
+			break;
+		case'd': cout << "D" << endl;
+			break;
+		case'e': cout << "E" << endl;
+			break;
+		case'f': cout << "F" << endl;
+			break;
+		case'g': cout << "G" << endl;
+			break;
+		case'h': cout << "H" << endl;
+			break;
+		case'i': cout << "I" << endl;
+			break;
+		case'j': cout << "J" << endl;
+			break;
+		case'k': cout << "K" << endl;
+			break;
+		case'l': cout << "L" << endl;
+			break;
+		case'm': cout << "M" << endl;
+			break;
+		case'n': cout << "N" << endl;
+			break;
+		case'o': cout << "O" << endl;
+			break;
+		case'p': cout << "P" << endl;
+			break;
+		case'q': cout << "Q" << endl;
+			break;
+		case'r': cout << "R" << endl;
+			break;
+		case's': cout << "S" << endl;
+			break;
+		case't': cout << "T" << endl;
+			break;
+		case'u': cout << "U" << endl;
+			break;
+		case'v': cout << "V" << endl;
+			break;
+		case'x': cout << "X" << endl;
+			break;
+		case'y': cout << "Y" << endl;
+			break;
+		case'z': cout << "Z" << endl;
+			break;
+		default:cout << "This input is not valid" << endl;
+			break;
+		}
+		return 0;
+		}
+
+
